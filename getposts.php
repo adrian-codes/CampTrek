10535b8ff88bb94b9af2c5056745fb2f5d5f1f6f
adrian-nieto <Nieto.Adrian87@gmail.com>
2015-04-03 18:08:59 -0700

    reading posts from db complete

--- getposts.php
+++ getposts.php
@@ -1,6 +1,6 @@
 <?php
     $con = mysqli_connect('localhost', 'root', '', 'camptrek');
-    $sql = 'SELECT * FROM post ORDER BY ID desc';
+    $sql = 'SELECT p.*, i.src_id, i.src_type, i.photoURL, i.photoDesc, i.author_id, you.username FROM `post` AS p JOIN `images` AS i ON p.id = i.src_id JOIN `users` AS you ON p.user_id = you.id';
     $results = mysqli_query($con, $sql);
    
     $outputArray = [];
@@ -17,7 +17,14 @@
         $summary = $post_row['summary'];
         $tips_tricks = $post_row['tips_tricks'];
         $user_id= $post_row['user_id'];
-        $like_count = $post_row['like_count']; 
+        $like_count = $post_row['like_count'];
+        $date = $post_row['date'];
+        $image_src_id = $post_row['src_id'];
+        $image_src_type = $post_row['src_type'];
+        $image_photoURL = $post_row['photoURL'];
+        $image_photoDesc = $post_row['photoDesc'];
+        $image_author_id = $post_row['author_id'];
+        $username = $post_row['username'];
 
         $data[] = [
             'id' => $id,
@@ -28,20 +35,28 @@
             'summary' => $summary,
             'tips_tricks' => $tips_tricks,
             'user_id' => $user_id,
-            'like_count' => $like_count
+            'like_count' => $like_count,
+            'date' => $date,
+            'src_id' => $image_src_id,
+            'src_type' => $image_src_type,
+            'photoURL' => $image_photoURL,
+            'photoDesc' => $image_photoDesc,
+            'author_id' => $image_author_id,
+            'username' => $username
         ];
         
     }
+
 
     if(mysqli_num_rows($results) > 0){
         $outputArray['success'] = true; 
         $outputArray['data'] = $data;
     }
-    else  
-    {
+    else  {
         $outputArray['success'] = false;
         $outputArray['message'] = "There was nothing retrieved!";
         
     }
+
     echo (json_encode($outputArray));
-?>  
+?> 
--- main.js
+++ main.js
@@ -19,10 +19,18 @@ $('document').ready(function(){
                 var time_created = response.data[index].time_created;
                 var tips_tricks = response.data[index].tips_tricks;
                 var user_id = response.data[index].user_id;
-           
+                var date = response.data[index].date;
+                var photoURL = response.data[index].photoURL;
+                var photoDesc = response.data[index].photoDesc;
+                var username = response.data[index].username;
                 
-                var postDiv = $('<div/>').addClass('postDiv');
-                $('<div class="infoDiv"><p>'+campground+'</p><p>'+category+'</p></div>').appendTo(postDiv);
+                
+                var postDiv = $('<div/>').addClass('postDiv row');
+                
+                $('<div class="infoDiv col-md-4" data-user="'+user_id+'" data-id="'+id+'"><p>User: '+username+'<p>Campground: '+campground+'</p><p>Date: '+date+'</p><p>Category: '+category+'</p><p>Rating: '+rating+'</p></div>').appendTo(postDiv);
+                
+                $('<div class="photoDiv col-md-8"><img src="'+photoURL+'" class="img-responsive" alt="image"><p>Description: '+photoDesc+'</p></div>').appendTo(postDiv);
+                $('<div class="summaryDiv col-md-12"><p>Summary: '+summary+'</p><p>Tips & Tricks: '+tips_tricks+'</div><hr class="col-md-12">').appendTo(postDiv);
                 postDiv.appendTo('.postContainer');
                 
             }
