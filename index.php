768e5320e67ce9df2fd1bad7aa4e5e5c8bc9b14d
adrian-nieto <Nieto.Adrian87@gmail.com>
2015-05-05 18:19:14 -0700

    mods to styling on front page and reviews

--- index.php
+++ index.php
@@ -13,21 +13,23 @@
         <?php include( 'includes/header.php'); ?>
     </div>
     <div class="container-fluid" id="bg-img">
-        <h1 class="text-center"><span class="textBackground">Welcome to CampTrek!<small> Let's plan your next trip!</small></span></h1>
-        <h4 class="text-center"><span class="textBackground">CampTrek is a platform for camping beginners and experts to find campsites, share photos, tips/tricks and reviews of campsites.</span></h4><br>
+        <h1 class="text-center">Welcome to CampTrek!<small> Let's plan your next trip!</small></h1>
         <div class="col-md-5 col-md-offset-1 textBackground">
-            <h3 class="text">Step 1: Find a campsite</h3>
-            
-            <h3>Step 2: Get Directions</h3>
-            
-            <h3>Step 3: Go camping!</h3>
+            <p>CampTrek is a platform for camping beginners and experts to find campsites, share photos, tips/tricks and reviews of campsites.</p>
+            <h3 class="text"><span class="label">Step 1:</span> <a href="findcampsites.php">Find a campsite</a></h3>
+
+            <h3><span class="label">Step 2:</span> <a href="reviews.php">Read Reviews</a></h3>
+
+            <h3><span class="label">Step 3:</span> <a href="http://www.lovetheoutdoors.com/camping/checklists.htm" target="_blank">Get Prepared</a></h3>
+            <h3><span class="label">Go Camping!</span></h3>
 
         </div>
         <div class="col-md-5 col-md-offset-1 textBackground">
             <div>
-            <h4>Become a part of the CampTrek community and help others by leaving reviews of campsites, posting pictures, and sharing your words of wisdom.</h4>
+                <h3>Join the Community</h3>
+                <p>Become a part of the CampTrek community and help others by leaving reviews of campsites, posting pictures, and sharing your words of wisdom.</p>
             </div>
-            <?php include('signup.php'); ?>
+            <?php include( 'signup.php'); ?>
         </div>
     </div>
     <?php include( 'includes/footer.php'); ?>
--- signup.php
+++ signup.php
@@ -2,19 +2,16 @@
 <form class="form-horizontal" action="registrationadd.php" method="post">
     <h3>Not a member? Sign up here!</h3>
     <div class="form-group">
-        <label for="inputUsername" class="col-md-2 control-label">Username</label>
         <div class="col-md-10">
             <input type="text" name="username" class="form-control" id="regUsername" placeholder="Username">
         </div>
     </div>
     <div class="form-group">
-        <label for="inputEmail" class="col-md-2 control-label">Email</label>
         <div class="col-md-10">
             <input type="email" name="useremail" class="form-control" id="regEmail" placeholder="Email">
         </div>
     </div>
     <div class="form-group">
-        <label for="inputPassword" class="col-md-2 control-label">Password</label>
         <div class="col-md-10">
             <input type="password" name="password" class="form-control" id="regPassword" placeholder="Password">
         </div>
